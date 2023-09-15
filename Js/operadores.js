6d062ef55852d5116d0bda8a0cea14a4857b6745
AlvaroICINF <alvaroibanez.icinf@gmail.com>
2023-09-15 12:36:00 -0300

    Create operadores.js
    trabajo en clases.

---
+++ Js/operadores.js
@@ -0,0 +1,22 @@
+//unario
+let z=1;
+z=-1;
+console.log(`el valor z es ${z}`);
+
+//Concatenación con el binario +
+//En este caso no aplica "2"/2=1
+console.log('1'+2);//el resultado es 12
+console.log(2+'1');//el resultado es 21
+console.log(1+1+'2');//el resultado es 22
+console.log('2'+1+1);//el resultado es 22
+
+console.log(+true);//devuelve como resultado 1 gracias al operador 1
+console.log(+"4");//transforma el string en numero
+
+//asignación encadenadas
+
+a=b=c=10
+
+//incremento decremento
+1++;
+1--;
