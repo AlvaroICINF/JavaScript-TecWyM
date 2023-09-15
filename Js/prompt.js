7fc42d44b5d21fbbc03ec527d9b0d1fe79603117
AlvaroICINF <alvaroibanez.icinf@gmail.com>
2023-09-15 12:36:09 -0300

    Create prompt.js
    trabajo en clases.

---
+++ Js/prompt.js
@@ -0,0 +1,9 @@
+//Prompt y confirm
+
+//prompt
+/*let edad =prompt("¿Cuál es tu edad?", 0);
+alert(`tienes ${edad} años`)*/
+
+//confirm
+let question=confirm('¿Pasarás el ramo?');
+alert(question)//opciones aceptar-cancelar
